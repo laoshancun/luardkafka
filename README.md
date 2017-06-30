3f4fe2bd2417e0a74be61119b2423078143d9df0
laoshancun <laoshancun@foxmail.com>
2017-06-30 11:50:36 +0800

    add ssl example

---
+++ README.md
@@ -0,0 +1,13 @@
+luardkafka client for Apache Kafka<sup>TM</sup>
+==============
+
+luardkafka is lua client for [Apache Kafka](http://kafka.apache.org/).
+
+Features:
+
+- **High performance** - luardkafka is a lightweight wrapper around
+[librdkafka](https://github.com/edenhill/librdkafka), a finely tuned C
+client.
+
+## Usage
+[example.lua](example.lua).
--- example.lua
+++ example.lua
@@ -29,3 +29,26 @@ end
 while producer:outq_len() ~= 0 do
     producer:poll(10)
 end
+
+-- using ssl
+local kafka_config = require 'rdkafka.config'.create()
+kafka_config["group.id"] =  "hualv-fes-static-file-generator"
+--config["enable.auto.commit"] =  "true"
+kafka_config["auto.commit.interval.ms"] =  "5000"
+kafka_config["bootstrap.servers"] =  "SSL://kafka-broker-1:9093,SSL://kafka-broker-2:9093"
+kafka_config["statistics.interval.ms"] =  "60000"
+kafka_config["security.protocol"] =  "SSL"
+kafka_config["ssl.ca.location"] =  "/path/to/ca.cer"
+kafka_config["ssl.certificate.location"] =  "/path/to/client-cert.cer"
+kafka_config["ssl.key.location"] =  "/path/to/client-key.pem"
+kafka_config["ssl.key.password"] =  "client-key-password"
+kafka_config:set_delivery_cb(function (payload, err) print("Delivery Callback '"..payload.."'") end)
+kafka_config:set_stat_cb(function (payload) print("Stat Callback '"..payload.."'") end)
+
+local producer = require 'rdkafka.producer'.create(kafka_config)
+
+local topic_config = require 'rdkafka.topic_config'.create()
+topic_config["auto.commit.enable"] = "true"
+topic = require 'rdkafka.topic'.create(producer, "static", topic_config)
+
+producer:produce(topic, -1, "message");
